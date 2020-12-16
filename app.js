ca31761c2d76c232d434b0a6fae35bb0ecbdbb95
MalluMalligereKumar <mallumkumar069@gmail.com>
2020-12-16 13:27:27 +1100

    Add POST route to /quotes and error handling codes

--- app.js
+++ app.js
@@ -2,20 +2,55 @@ const express = require('express');
 const app = express();
 const records = require('./records');
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.json({ greeting: "Hello Welcome!!" });
+
+    try {
+        res.json({ greeting: "Hello Welcome!!" });
+    } catch (err) {
+        res.json({ message: err.message });
+    }
+
 });
 
 // Send a GET request to /quotes to read or view all the quotes.
 app.get('/quotes', async (req, res) => {
-    const quotes = await records.getQuotes();
-    res.json(quotes);
+
+    try {
+        const quotes = await records.getQuotes();
+        res.json(quotes);
+    } catch (err) {
+        res.json({ message: err.message });
+    }
+
 });
 
 // Send a GET request to /quotes/:id to read or view a quote.
 app.get('/quotes/:id', async (req, res) => {
-    const recordData = await records.getQuote(req.params.id);
-    res.json(recordData);
+
+    try {
+        const recordData = await records.getQuote(req.params.id);
+        res.json(recordData);
+    } catch (err) {
+        res.json({ message: err.message });
+    }
+
+});
+
+// Send a POST request to /quotes to create or add a new quote.
+app.post('/quotes', async (req, res) => {
+
+    try {
+        const newQuote = await records.createQuote({
+            quote: req.body.quote,
+            author: req.body.author
+        });
+        res.json(newQuote);
+    } catch (err) {
+        res.json({ message: err.message });
+    }
+
 });
 
 app.listen(3000, () => console.log('API listening on localhost:3000'));
