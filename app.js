ee2d3c34b9339cf8afed3a80d8094639fe41e9ef
MalluMalligereKumar <mallumkumar069@gmail.com>
2020-12-16 11:30:42 +1100

    Add info to the root route

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => console.log('Quote API listening on port 3000!'));
+app.get('/', (req, res) => {
+    res.json({ greeting: "Hello Welcome!!" });
+});
+
+app.listen(3000, () => console.log('API listening on localhost:3000'));
+
+const data = {
+    records: [
+        {
+            "id": 8721,
+            "quote": "We must accept finite disappointment, but we must never lose infinite hope.",
+            "author": "Martin Luther King"
+        },
+        {
+            "id": 5779,
+            "quote": "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
+            "author": "Yvonne Pierre"
+        },
+        {
+            "id": 3406,
+            "quote": "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
+            "author": "Barbara DeAngelis"
+        }
+    ]
+}
