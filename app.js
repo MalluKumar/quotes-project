7715f52c843fd9d1212c19f901680d22bddea219
MalluMalligereKumar <mallumkumar069@gmail.com>
2020-12-16 12:23:18 +1100

    Modified the /quotes and /quotes/:id routes to use data store records

--- app.js
+++ app.js
@@ -1,40 +1,21 @@
 const express = require('express');
 const app = express();
+const records = require('./records');
 
 app.get('/', (req, res) => {
     res.json({ greeting: "Hello Welcome!!" });
 });
 
 // Send a GET request to /quotes to read or view all the quotes.
-app.get('/quotes', (req, res) => {
-    res.json(data);
+app.get('/quotes', async (req, res) => {
+    const quotes = await records.getQuotes();
+    res.json(quotes);
 });
 
 // Send a GET request to /quotes/:id to read or view a quote.
-app.get('/quotes/:id', (req, res) => {
-    const id = req.params.id;
-    const quote = data.records.find(record => record.id == id);
-    res.json(quote);
+app.get('/quotes/:id', async (req, res) => {
+    const recordData = await records.getQuote(req.params.id);
+    res.json(recordData);
 });
 
 app.listen(3000, () => console.log('API listening on localhost:3000'));
-
-const data = {
-    records: [
-        {
-            "id": 8721,
-            "quote": "We must accept finite disappointment, but we must never lose infinite hope.",
-            "author": "Martin Luther King"
-        },
-        {
-            "id": 5779,
-            "quote": "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
-            "author": "Yvonne Pierre"
-        },
-        {
-            "id": 3406,
-            "quote": "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
-            "author": "Barbara DeAngelis"
-        }
-    ]
-}
